249d823946783f4d6f37b658f66035936fe1ca7d
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-11 12:07:40 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,6 @@
 def square_array(array)
   # your code here
-  array.each do |number| # do begins a block
-    # the lines between the do/end are the block's body
-    puts "#{number}"
-  end # end terminates the block
+  array.each do |number| 
+    number **
+  end 
 end
